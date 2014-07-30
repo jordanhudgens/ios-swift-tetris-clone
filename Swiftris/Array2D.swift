b27183a631c0f1e4c1818ca8945fc4288ee0493c
Jordan Hudgens <jordan@wow.com>
2014-07-29 18:25:16 -0600

    integrated an array class

---
+++ Swiftris/Array2D.swift
@@ -0,0 +1,31 @@
+//
+//  Array2D.swift
+//  Swiftris
+//
+//  Created by Jordan Hudgens on 7/29/14.
+//  Copyright (c) 2014 Jordan Hudgens. All rights reserved.
+//
+
+// #1
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        
+        array = Array<T?>(count:rows * columns, repeatedValue: nil)
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
