2db4c6b3a1a8a09954b162bf9e6935f200d93cf4
Jordan Hudgens <jordan@wow.com>
2014-08-05 11:34:45 -0700

    added the ability for pan gesture recognizer to move blocks from side to side

--- Swiftris/GameViewController.swift
+++ Swiftris/GameViewController.swift
@@ -14,6 +14,8 @@ class GameViewController: UIViewController, SwiftrisDelegate, UIGestureRecognize
     
     var scene: GameScene!
     var swiftris:Swiftris!
+    
+    var panPointReference:CGPoint?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -68,6 +70,23 @@ class GameViewController: UIViewController, SwiftrisDelegate, UIGestureRecognize
     
     @IBAction func didTap(sender: UITapGestureRecognizer) {
         swiftris.rotateShape()
+    }
+    
+    @IBAction func didPan(sender: UIPanGestureRecognizer) {
+        let currentPoint = sender.translationInView(self.view)
+        if let originalPoint = panPointReference {
+            if abs(currentPoint.x - originalPoint.x) > (BlockSize * 0.9) {
+                if sender.velocityInView(self.view).x > CGFloat(0) {
+                    swiftris.moveShapeRight()
+                    panPointReference = currentPoint
+                } else {
+                    swiftris.moveShapeLeft()
+                    panPointReference = currentPoint
+                }
+            }
+        } else if sender.state == .Began {
+            panPointReference = currentPoint
+        }
     }
     
     func gameDidEnd(swiftris: Swiftris) {
